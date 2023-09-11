2309c53f3aa3863b88f5cd479d42905c18d6758a
Dinara M <d9268843743@yandex.ru>
2023-09-11 14:05:31 +0200

    Проектная работа 14.3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { celebrate } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
@@ -20,8 +20,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 app.use('/users', auth, usersRouter);
 app.use('/cards', auth, cardsRouter);
-app.post('/signin', celebrate(validation.userSchema), login);
-app.post('/signup', celebrate(validation.userSchema), createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().uri().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
