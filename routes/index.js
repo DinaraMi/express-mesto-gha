65455c325908ba0901bdec06e5f2ca43919e61df
Dinara M <d9268843743@yandex.ru>
2023-09-15 12:44:25 +0200

    Проектная работа 14.19

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
-const signinRouter = require('./signin');
-const signupRouter = require('./signup');
+const loginRouter = require('./login');
+const registrRouter = require('./registr');
 const NotFoundError = require('../errors/NotFoundError');
 const auth = require('../middlewares/auth');
 
-router.use('/signin', signinRouter);
-router.use('/signup', signupRouter);
+router.use('/signin', loginRouter);
+router.use('/signup', registrRouter);
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
 router.use('*', (req, res, next) => {
