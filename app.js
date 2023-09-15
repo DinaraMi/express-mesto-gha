3b9befd54cfee5cc3940d3809f2c3656051ca6d2
Dinara M <d9268843743@yandex.ru>
2023-09-15 14:36:00 +0200

    Проектная работа 14.21

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const indexRouter = require('./routes/index');
 const { handleErrors } = require('./middlewares/handleErrors');
 
 const { PORT = 3000 } = process.env;
@@ -16,7 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.get('/', (req, res) => {
   res.send('Запрос прошел успешно');
 });
-app.use('/', require('./routes/index'));
+app.use('/', indexRouter);
 
 app.use(errors());
 app.use(handleErrors);
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 const router = require('express').Router();
-const usersRouter = require('./users');
-const cardsRouter = require('./cards');
 const loginRouter = require('./login');
 const registrRouter = require('./registr');
+const usersRouter = require('./users');
+const cardsRouter = require('./cards');
+
 const NotFoundError = require('../errors/NotFoundError');
 const auth = require('../middlewares/auth');
 
-router.use('/signin', loginRouter);
-router.use('/signup', registrRouter);
+router.post('/signin', loginRouter);
+router.post('/signup', registrRouter);
+
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
+
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
